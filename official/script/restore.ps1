5c3e072d6b773be04b4491ac38b045c3000b76ff
Benknightdark <moon791017@gmail.com>
2017-12-15 14:05:51 +0800

    add restore db script

---
+++ official/script/restore.ps1
@@ -0,0 +1,2 @@
+docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=P@ssw0rd'  --name 'sql1' -e 'MSSQL_PID=Enterprise' -p 1401:1433  -v sql1data:/var/opt/mssql  -d official
+docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd  -S localhost -U SA -P "P@ssw0rd"    -Q  "RESTORE DATABASE BookStoreAppDB FROM DISK = '/var/opt/mssql/data/test.bak' WITH MOVE 'test_Data' TO '/var/opt/mssql/data/test.mdf',  MOVE 'test_Log' TO '/var/opt/mssql/data/test.ldf'"
